26ee17081732eaf75925e7c90d6405909dbdc841
pabloBolivar <83661463+pabloBolivar@users.noreply.github.com>
2021-08-05 16:10:10 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import React from "react";
+import { Provider } from "react-redux";
+
+import { store } from "./redux/store";
 import "./styles.css";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <Provider store={store}>
+      <React.Fragment></React.Fragment>
+    </Provider>
+  );
 }
